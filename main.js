b4d4c66f65ee4341be5faa20d9a53e2e219bd6e6
resq-u <jakub12lepianka@wp.pl>
2024-08-01 11:03:53 +0200

    Late commit. Mostly complete html and css files. Create .js file

---
+++ main.js
@@ -0,0 +1,7 @@
+const form = document.querySelector("form");
+const name = document.getElementById("user_name");
+const surname = document.getElementById("user_surname");
+const email = document.getElementById("user_email");
+const tel = document.getElementById("user_tel");
+const pwd = document.getElementById("user_password");
+
