bf4d1f9b214dd2a83ebdda146cdc419c9922cd3a
resq-u <jakub12lepianka@wp.pl>
2024-08-04 13:49:24 +0200

    combine focus and validity color changes for input fields

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const form = document.querySelector("form");
-const name = document.getElementById("user_name");
-const surname = document.getElementById("user_surname");
-const email = document.getElementById("user_email");
-const tel = document.getElementById("user_tel");
-const pwd = document.getElementById("user_password");
 
+const inputElements = [
+    document.getElementById("user_name"),
+    document.getElementById("user_surname"),
+    document.getElementById("user_email"),
+    document.getElementById("user_tel"),
+    document.getElementById("user_password")
+];
+
+const nameError = document.querySelector("#user_name + span.name_error");
+const surnameError = document.querySelector("#user_surname + span.surname_error");
+const emailError = document.querySelector("#user_email + span.email_error");
+const telError = document.querySelector("#user_tel + span.tel_error");
+const pwdError = document.querySelector("#user_password + span.pwd_error");
+
+inputElements.forEach(input => {
+    input.addEventListener("keyup", (event) => {
+        if (input.value.length > 0) {
+            input.className = "";
+        }
+    });
+});
